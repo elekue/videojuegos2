3e9a42ee781eb0a6a0cd3b4541376cddcdff34ce
Elisabet Lekue <Elekue@fptxurdinaga.com>
2025-05-15 23:04:25 +0200

    Añadidas migraciones de campeonatos, jugadores y participaciones

---
+++ database/migrations/2025_05_15_205530_create_jugadores_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jugadores', function (Blueprint $table) {
+            $table->id();
+            $table->string('nick')->unique();
+            $table->string('nombre');
+            $table->string('correo')->unique();
+            $table->string('telefono');
+            $table->string('nacionalidad');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jugadores');
+    }
+};
